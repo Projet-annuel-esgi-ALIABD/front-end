44e4a9df2370cf0b946ba300297b7a2fb2d4670d
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2025-04-24 16:38:51 +0200

    verification routing

--- src/router.ts
+++ src/router.ts
@@ -4,23 +4,19 @@ import NotFound from './pages/NotFound.vue';
 import Auth from './pages/Auth.vue';
 import Healthcheck from './pages/Healthcheck.vue';
 
-
-const routes = [
-  { path: '/', component: Index },
-  { path: '/health', component: Healthcheck},
-  { path: '/map', component: NotFound },
-  { path: '/settings', component: NotFound },
-  { path: '/notifications', component: NotFound },
-  { path: '/auth', component: Auth },
-  { path: '/forgot-password', component: NotFound },
-  // ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE
-  { path: '/:pathMatch(.*)*', component: NotFound }
-]
-
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    { path: '/', component: Index },
+    { path: '/health', component: Healthcheck},
+    { path: '/map', component: NotFound },
+    { path: '/settings', component: NotFound },
+    { path: '/notifications', component: NotFound },
+    { path: '/auth', component: Auth },
+    { path: '/forgot-password', component: NotFound },
+    // ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE
+    { path: '/:pathMatch(.*)*', component: NotFound }
+  ]
 });
 
 export default router;
